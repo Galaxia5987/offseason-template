9be89e27a8ded8783bcdbe746b46ef1026a645e6
rakrakon <68773850+rakrakon@users.noreply.github.com>
2025-07-05 11:48:17 +0300

    Add UniversalMotor

---
+++ src/main/kotlin/frc/robot/lib/universal_motor/MotorIOReal.kt
@@ -0,0 +1,33 @@
+package frc.robot.lib.universal_motor
+
+import com.ctre.phoenix6.configs.TalonFXConfiguration
+import com.ctre.phoenix6.controls.ControlRequest
+import com.ctre.phoenix6.hardware.TalonFX
+import edu.wpi.first.units.measure.Distance
+import frc.robot.lib.extensions.toDistance
+
+class MotorIOReal(
+    private val port: Int,
+    private val canBus: String,
+    override val config: TalonFXConfiguration,
+    private val gearRatio: Double,
+    private val radius: Distance
+) : MotorIO {
+    override val inputs = LoggedMotorInputs()
+    private val motor = TalonFX(port, canBus)
+
+    init {
+        motor.configurator.apply(config)
+    }
+
+    override fun setRequest(controlRequest: ControlRequest) {
+        motor.setControl(controlRequest)
+    }
+
+    override fun updateInputs() {
+        inputs.current = motor.supplyCurrent.value
+        inputs.position = motor.position.value
+        inputs.voltage = motor.motorVoltage.value
+        inputs.distance = motor.position.value.toDistance(radius, gearRatio)
+    }
+}
