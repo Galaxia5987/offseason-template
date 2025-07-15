74abeb40225c136a364b538c08fc331fcc7876fd
AdarWa <adar.wasserman@gmail.com>
2025-07-15 18:55:04 +0300

    Apply Spotless

--- src/main/kotlin/frc/robot/subsystems/roller/Roller.kt
+++ src/main/kotlin/frc/robot/subsystems/roller/Roller.kt
@@ -10,24 +10,21 @@ import org.littletonrobotics.junction.Logger
 
 class Roller : SubsystemBase() {
 
-    private val motor: UniversalTalonFX = UniversalTalonFX(
-        MOTOR_ID,
-        "",
-        MOTOR_CONFIG
-    )
+    private val motor: UniversalTalonFX =
+        UniversalTalonFX(MOTOR_ID, "", MOTOR_CONFIG)
 
     private val sensor = AnalogInput(0)
 
     private val voltageRequest = VoltageOut(0.0)
 
     private fun setVoltage(voltage: Voltage): Command =
-        runOnce {
-            motor.setControl(voltageRequest.withOutput(voltage))
-        }.withName("Roller/setVoltage")
+        runOnce { motor.setControl(voltageRequest.withOutput(voltage)) }
+            .withName("Roller/setVoltage")
 
     fun intake(): Command = setVoltage(INTAKE_VOLTAGE).withName("Roller/intake")
 
-    fun outtake(): Command = setVoltage(OUTTAKE_VOLTAGE).withName("Roller/outtake")
+    fun outtake(): Command =
+        setVoltage(OUTTAKE_VOLTAGE).withName("Roller/outtake")
 
     override fun periodic() {
         motor.updateInputs()
