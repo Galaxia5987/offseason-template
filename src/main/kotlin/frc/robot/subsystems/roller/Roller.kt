0406a74fe22ef348b60a8d1e1d2bc814b85a54e3
AdarWa <adar.wasserman@gmail.com>
2025-07-17 18:56:51 +0300

    Update Roller to handle the intake and outtake using `StartEndCommand`

--- src/main/kotlin/frc/robot/subsystems/roller/Roller.kt
+++ src/main/kotlin/frc/robot/subsystems/roller/Roller.kt
@@ -2,18 +2,18 @@ package frc.robot.subsystems.roller
 
 import com.ctre.phoenix6.controls.VoltageOut
 import edu.wpi.first.units.measure.Voltage
-import edu.wpi.first.wpilibj.AnalogInput
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.SubsystemBase
+import frc.robot.lib.StartEndCommand
+import frc.robot.lib.extensions.kg2m
+import frc.robot.lib.extensions.volts
 import frc.robot.lib.universal_motor.UniversalTalonFX
 import org.littletonrobotics.junction.Logger
 
 class Roller : SubsystemBase() {
 
     private val motor: UniversalTalonFX =
-        UniversalTalonFX(MOTOR_ID, "", MOTOR_CONFIG)
-
-    private val sensor = AnalogInput(0)
+        UniversalTalonFX(MOTOR_ID, "", MOTOR_CONFIG, 0.3.kg2m)
 
     private val voltageRequest = VoltageOut(0.0)
 
@@ -21,11 +21,17 @@ class Roller : SubsystemBase() {
         runOnce { motor.setControl(voltageRequest.withOutput(voltage)) }
             .withName("Roller/setVoltage")
 
-    fun intake(): Command = setVoltage(INTAKE_VOLTAGE).withName("Roller/intake")
+    fun intake(): Command =
+        this.setVoltage(INTAKE_VOLTAGE).withName("Roller/intake")
+
+    fun stop(): Command = setVoltage(0.0.volts).withName("Roller/stop")
 
     fun outtake(): Command =
-        setVoltage(OUTTAKE_VOLTAGE).withName("Roller/outtake")
+        this.setVoltage(OUTTAKE_VOLTAGE).withName("Roller/outtake")
 
+    fun handleIntake(): Command = StartEndCommand(intake(), stop())
+
+    //    fun handleIntake(): Command = PrintCommand("ASdasasdasda")
     override fun periodic() {
         motor.updateInputs()
         Logger.processInputs("Roller", motor.inputs)
