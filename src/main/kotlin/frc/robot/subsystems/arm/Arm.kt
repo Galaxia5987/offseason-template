d95d003c8e53614f2d145d7523154ad1310dd70c
AdarWa <adar.wasserman@gmail.com>
2025-07-17 19:32:16 +0300

    Add Ligament to `Arm` Subsystem.

--- src/main/kotlin/frc/robot/subsystems/arm/Arm.kt
+++ src/main/kotlin/frc/robot/subsystems/arm/Arm.kt
@@ -5,12 +5,20 @@ import edu.wpi.first.units.measure.Angle
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 import frc.robot.lib.extensions.deg
+import frc.robot.lib.extensions.get
 import frc.robot.lib.universal_motor.UniversalTalonFX
 import org.littletonrobotics.junction.Logger
+import org.littletonrobotics.junction.mechanism.LoggedMechanism2d
+import org.littletonrobotics.junction.mechanism.LoggedMechanismLigament2d
 
 class Arm : SubsystemBase() {
     private val motor: UniversalTalonFX =
         UniversalTalonFX(MOTOR_ID, config = MOTOR_CONFIG)
+
+    private val mechanism = LoggedMechanism2d(3.0, 3.0)
+    private val arm = mechanism.getRoot("arm", 2.0, 2.0)
+    private val ligament =
+        arm.append(LoggedMechanismLigament2d("arm", 1.0, 90.0))
 
     private var setpoint: Angle = 0.deg
 
@@ -32,5 +40,9 @@ class Arm : SubsystemBase() {
         motor.updateInputs()
         Logger.processInputs(this::class.simpleName, motor.inputs)
         Logger.recordOutput("Arm/Setpoint", setpoint)
+
+        ligament.setAngle(motor.inputs.position[deg])
+
+        Logger.recordOutput("Arm/Mechanism", mechanism)
     }
 }
