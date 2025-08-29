f2f66ea69f5ba8a53b0a0007f03cf900d61a7b21
AlonSchwierz <alon.schwierz@gmail.com>
2025-08-29 10:51:55 +0300

    random upgrates

--- src/main/kotlin/frc/robot/RandomStuff.kt
+++ src/main/kotlin/frc/robot/RandomStuff.kt
@@ -2,8 +2,6 @@ package frc.robot
 
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.Commands
-import frc.robot.lib.extensions.degreesPerSecond
-import frc.robot.subsystems.flywheel.Flywheel
 
 object Feeder{
     fun releaseToShooter(): Command {
@@ -13,7 +11,6 @@ object Feeder{
 fun fullSequence() : Command {
     return Commands.sequence(
         Robot.hood.steepestAngle(),
-        Robot.flywheel.rampToVelocity(1000.degreesPerSecond),
         Feeder.releaseToShooter()
     )
 }
