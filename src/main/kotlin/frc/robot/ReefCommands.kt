7f52404c74d2e3ec83a079ba005820d8f3eec92f
AlonSchwierz <alon.schwierz@gmail.com>
2025-10-03 13:53:06 +0300

    Fix an mistake in the command use and making it work

--- src/main/kotlin/frc/robot/ReefCommands.kt
+++ src/main/kotlin/frc/robot/ReefCommands.kt
@@ -1,20 +1,19 @@
 package frc.robot
 
 import edu.wpi.first.wpilibj2.command.Command
-import edu.wpi.first.wpilibj2.command.Commands
 
 fun goToL1(): Command {
-    return Commands.runOnce({ elevator.GoToL1().alongWith(wrist.moveToL1()) })
+    return  elevator.GoToL1().alongWith(wrist.moveToL1())
 }
 
 fun goToL2(): Command {
-    return Commands.runOnce({ elevator.GoToL2().alongWith(wrist.moveToL2()) })
+    return  elevator.GoToL2().alongWith(wrist.moveToL2())
 }
 
 fun goToL3(): Command {
-    return Commands.runOnce({ elevator.GoToL3().alongWith(wrist.moveToL3()) })
+    return elevator.GoToL3().alongWith(wrist.moveToL3())
 }
 
 fun goToL4(): Command {
-    return Commands.runOnce({ elevator.GoToL4().alongWith(wrist.moveToL4()) })
+    return elevator.GoToL4().alongWith(wrist.moveToL4())
 }
