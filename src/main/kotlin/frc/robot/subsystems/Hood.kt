15eb6d7becbfc9e3b755bc69c36808c87516ebeb
Elinor <elinor6almog@gmail.com>
2025-10-21 17:04:03 +0300

    Create Hood

---
+++ src/main/kotlin/frc/robot/subsystems/Hood.kt
@@ -0,0 +1,22 @@
+package frc.robot.subsystems
+
+import com.ctre.phoenix6.controls.PositionVoltage
+import com.ctre.phoenix6.hardware.TalonFX
+import edu.wpi.first.units.measure.Angle
+import edu.wpi.first.wpilibj2.command.SubsystemBase
+import frc.robot.lib.extensions.degrees
+import frc.robot.lib.extensions.get
+
+class Hood : SubsystemBase() {
+    val motorHood: TalonFX = TalonFX(3)
+    val positionReq: PositionVoltage = PositionVoltage(0.0)
+    var setPoint: Double = 0.0
+    fun setAngle(angle: Angle) {
+        setPoint = angle[degrees]
+        motorHood.setControl(positionReq.withPosition(angle[degrees]))
+    }
+
+    override fun periodic() {
+        super.periodic()
+    }
+}
